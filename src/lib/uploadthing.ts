2fe986a626d06b8d16e2ee345f97232b49eb97ea
Diego <diegoh.code@outlook.com>
2024-12-17 12:59:34 -0800

    uploadthing rework

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,6 +1,25 @@
 import { generateReactHelpers } from "@uploadthing/react";
+import { createUploadthing, type FileRouter } from "uploadthing/express";
 
-import type { OurFileRouter } from "../../../lib/uploadthing";
+const f = createUploadthing();
+
+export const uploadRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({
+    image: {
+      /**
+       * For full list of options and defaults and defaults, see the File Route API reference
+       * @see https://docs.uploadthing.com/file-routes#route-config
+       */
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  }).onUploadComplete((data) => {
+    console.log("upload completed", data);
+  }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof uploadRouter;
 
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>();
